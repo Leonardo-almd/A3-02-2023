3fa79b42e0406fc87e56dc40d78fe411fdcc274e
Leonardo-almd <leonardo.alv1@hotmail.com>
2023-10-16 20:12:53 -0300

    entrega-02

---
+++ migrations/database.sql
@@ -0,0 +1,40 @@
+CREATE TABLE usuarios (
+    _id INT PRIMARY KEY,
+    nome VARCHAR(255),
+    data_nascimento DATE,
+    cpf VARCHAR(255),
+    endereco VARCHAR(255),
+    email VARCHAR(255),
+    telefone VARCHAR(255),
+    qtd_emprestismos INT    
+);
+
+CREATE TABLE livros (
+    _id INT PRIMARY KEY,
+    titulo VARCHAR(255),
+    autor VARCHAR(255),
+    editora VARCHAR(255),
+    ano_lancamento INT,
+    genero VARCHAR(255),
+    qtd_paginas INT,
+    qtd_edicao INT,
+    status_reserva BOOLEAN
+)
+
+CREATE TABLE funcionarios (
+    _id INT PRIMARY KEY,
+    nome VARCHAR(255),
+    cpf VARCHAR(255),
+    data_contratacao DATE,
+    cargo VARCHAR(255),
+    email VARCHAR(255),
+    telefone VARCHAR(255)
+)
+
+CREATE TABLE reservas (
+    _id INT PRIMARY KEY,
+    livro INT,
+    data_reserva DATE,
+    data_expiracao DATE,
+    usuario INT
+)
